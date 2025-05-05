0393447f021b689d5f8a34e0529be5b9bd887d5b
Emsouza01 <63312236+Emsouza01@users.noreply.github.com>
2025-05-05 20:40:26 +0000

    ajustes no vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+    base: '/beleza-f/',
   server: {
     host: "::",
     port: 8080,
@@ -19,5 +20,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/beleza-f.git/',
 }));
